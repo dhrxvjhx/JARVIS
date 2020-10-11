2a13cc07623b0ca59a6201a334f369ae03271605
stifler68 <52568505+stifler68@users.noreply.github.com>
2020-10-12 00:54:12 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -17,7 +17,7 @@ import datetime
 import speech_recognition as sr
 import wikipedia
 import webbrowser   #Standard Library(No need to install)
-#import os
+import os
 
 #importing pyttsx3
 engine = pyttsx3.init('sapi5')
@@ -28,6 +28,23 @@ engine.setProperty('voice', voices[0].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+    
+def Increase_brightness():
+    try:
+        currrent_brightness=sbc.get_brightness()
+        increased_brightness=currrent_brightness+10
+        sbc.set_brightness(increased_brightness)
+    except Exception as e:
+        speak("sir brightness is full")
+
+def Decrease_brightness():
+    try:
+        currrent_brightness=sbc.get_brightness()
+        decreased_brightness=currrent_brightness-10
+        sbc.set_brightness(decreased_brightness)
+    except Exception as e:
+        speak("sir brightness is low")
+
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
@@ -116,6 +133,52 @@ if __name__ == "__main__":
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             print(strTime)
             speak(strTime)
+            
+        elif "show battery status" in query:
+            battery=psutil.sensors_battery()
+            percent = str(battery.percent)
+            plugged=battery.power_plugged
+            #plugged = "Plugged In" if plugged else "Not Plugged In"
+            if plugged:
+                plug='plugged'
+            else:
+                plug='not plugged'
+            per = int(percent)
+            # plugged = "Plugged In" if plugged else "Not Plugged In"
+            if plugged:
+                plug = "plugged"
+            else:
+                plug = "not_plugged"
+
+            if plug == 'not_plugged' and per <= 30:
+                speak("Sir your battery is ")
+                speak(percent)
+                speak("sir please pluggin your charger because your battery is low ")
+            elif plug == 'plugged' and battery > 20:
+                speak("Sir your battery is ")
+                speak(percent)
+            elif plug == 'not_plugged' and per > 20:
+                speak("Sir your battery is ")
+                speak(percent)
+            elif plug == 'plugged' and battery <= 20:
+                speak("Sir your battery is ")
+                speak(percent)
+                speak("sir your battery is low dont remove your charger")
+            elif plug == 'plugged' and battery == 100:
+                speak("Sir your battery is ")
+                speak(percent)
+                speak("sir your battery is full please remove charger ")
+            else:
+                print(percent)    
+            
+         elif "increase the brightness" in query or "yes increase the brightness" in query or "increase brightness" in query:
+            Increase_brightness()
+            speak("sir should i increase the brightness or its good")
+
+        elif "decrease the brightness" in query or "yes decrease the brightness" in query or "decrease brightness" in query:
+            Decrease_brightness()
+            speak("sir should i decrease the brightness or its good")
+
         
         elif 'exit' in query:
             print('See you soon')
